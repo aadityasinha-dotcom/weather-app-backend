10f31b6134bdbf4b04cdf3954538f53050a7fcf6
aadityasinha-dotcom <aadityasinha009@gmail.com>
2023-01-26 19:27:08 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -17,9 +17,45 @@ function makeApiCall(location, format, u, API_KEY){
   return axios.get(url);
 }
 
-app.get("/api", (req, res) => {
-  const location = req.query.location;
+app.get("/api/weather", (req, res) => {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  const options = {
+    method: 'GET',
+    url: 'https://weatherbit-v1-mashape.p.rapidapi.com/current',
+    params: {lon: lon, lat: lat},
+    headers: {
+      'X-RapidAPI-Key': 'f3b4a1ce9cmshe9e8ade6b32071bp1782adjsn5aa35a72a16b',
+      'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+    }
+  };
+  axios.request(options).then(function (response) {
+    res.send(response.data);
+  }).catch(function (error) {
+      console.error(error);
+  });
+});
 
+app.get("/api/weather_by_location", (req, res) => {
+  const location = req.query.location;
+  const options = {
+    method: 'GET',
+    url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+    params: {city: location},
+    headers: {
+      'X-RapidAPI-Key': 'f3b4a1ce9cmshe9e8ade6b32071bp1782adjsn5aa35a72a16b',
+      'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+  };
+  axios.request(options).then(function (response) {
+    res.send(response.data);
+  }).catch(function (error) {
+      console.error(error);
+  });
+});
+
+app.get("/api/location", (req, res) => {
+  const location = req.query.location;
   const options = {
     method: 'GET',
     url: 'https://weather338.p.rapidapi.com/locations/search',
@@ -29,27 +65,11 @@ app.get("/api", (req, res) => {
       'X-RapidAPI-Host': 'weather338.p.rapidapi.com'
     }
   };
-
-  /*const options = {
-    method: 'GET',
-    url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
-    params: {city: location},
-    headers: {
-      'X-RapidAPI-Key': 'f3b4a1ce9cmshe9e8ade6b32071bp1782adjsn5aa35a72a16b',
-      'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-    }
-  };*/
-
-  var long=0;
-  var lat=0;
-  
   axios.request(options).then(function (response) {
-    res.send(response.data.latitude);
-    console.log(response.data);
+    res.send(response.data);
   }).catch(function (error) {
       console.error(error);
   });
-
 });
 
 
